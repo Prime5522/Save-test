da0238351e0abd60aa40fae0a3c84001ebaf0499
PRIME BOT'z <sahidhasannayem@gmail.com>
2025-03-19 04:30:20 +0600

    Update shrink.py

--- devgagan/modules/shrink.py
+++ devgagan/modules/shrink.py
@@ -160,7 +160,12 @@ async def about_callback(client, callback_query):
 ▸ ʙᴏᴛ sᴇʀᴠᴇʀ : <a href="https://heroku.com">ʜᴇʀᴏᴋᴜ</a>
 ▸ ʙᴜɪʟᴅ sᴛᴀᴛᴜs : v2.7.1 [sᴛᴀʙʟᴇ]"""
 
-await message.reply_text(text, parse_mode="HTML", disable_web_page_preview=True)
+await message.reply_text(
+    text=about_text, 
+    parse_mode="HTML",
+    disable_web_page_preview=True
+    )
+
 # ✅ রিফ্রেশ বাটনের ফাংশন
 @app.on_callback_query(filters.regex("refresh_check"))  
 async def refresh_callback(client: Client, query: CallbackQuery):  
